16b29027a14ec5a65a10e5ff9b221ff2b2fb73b2
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:56:49 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "qumulo_file_systems" {
+  description = "All qumulo_file_system resources"
+  value       = azurerm_qumulo_file_system.qumulo_file_systems
+}
+
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "qumulo_file_systems" {
+  description = <<EOT
+Map of qumulo_file_systems, attributes below
+Required:
+    - admin_password
+    - email
+    - location
+    - name
+    - resource_group_name
+    - storage_sku
+    - subnet_id
+    - zone
+Optional:
+    - offer_id
+    - plan_id
+    - publisher_id
+    - tags
+EOT
+
+  type = map(object({
+    admin_password      = string
+    email               = string
+    location            = string
+    name                = string
+    resource_group_name = string
+    storage_sku         = string
+    subnet_id           = string
+    zone                = string
+    offer_id            = optional(string, "qumulo-saas-mpp")
+    plan_id             = optional(string, "azure-native-qumulo-v3")
+    publisher_id        = optional(string, "qumulo1584033880660")
+    tags                = optional(map(string))
+  }))
+}
+
