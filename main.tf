6b1e005c828d0b0571da35810ebeabd70679c550
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:11 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+data "azurerm_key_vault_secret" "admin_password" {
+  for_each     = { for k, v in var.qumulo_file_systems : k => v if v.admin_password_key_vault_id != null && v.admin_password_key_vault_secret_name != null }
+  name         = each.value.admin_password_key_vault_secret_name
+  key_vault_id = each.value.admin_password_key_vault_id
+}
 resource "azurerm_qumulo_file_system" "qumulo_file_systems" {
   for_each = var.qumulo_file_systems
 
-  admin_password      = each.value.admin_password
+  admin_password      = each.value.admin_password != null ? each.value.admin_password : try(data.azurerm_key_vault_secret.admin_password[each.key].value, null)
   email               = each.value.email
   location            = each.value.location
   name                = each.value.name
--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "qumulo_file_systems" {
 Map of qumulo_file_systems, attributes below
 Required:
     - admin_password
+    - admin_password_key_vault_id (alternative to admin_password - read from Key Vault instead)
+    - admin_password_key_vault_secret_name (alternative to admin_password - read from Key Vault instead)
     - email
     - location
     - name
@@ -18,18 +20,20 @@ Optional:
 EOT
 
   type = map(object({
-    admin_password      = string
-    email               = string
-    location            = string
-    name                = string
-    resource_group_name = string
-    storage_sku         = string
-    subnet_id           = string
-    zone                = string
-    offer_id            = optional(string) # Default: "qumulo-saas-mpp"
-    plan_id             = optional(string) # Default: "azure-native-qumulo-v3"
-    publisher_id        = optional(string) # Default: "qumulo1584033880660"
-    tags                = optional(map(string))
+    admin_password                       = string
+    admin_password_key_vault_id          = optional(string)
+    admin_password_key_vault_secret_name = optional(string)
+    email                                = string
+    location                             = string
+    name                                 = string
+    resource_group_name                  = string
+    storage_sku                          = string
+    subnet_id                            = string
+    zone                                 = string
+    offer_id                             = optional(string) # Default: "qumulo-saas-mpp"
+    plan_id                              = optional(string) # Default: "azure-native-qumulo-v3"
+    publisher_id                         = optional(string) # Default: "qumulo1584033880660"
+    tags                                 = optional(map(string))
   }))
 }
 
