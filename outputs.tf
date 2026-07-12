988d8f0f397c6eb79091d4d8cd425f85ba5c68f3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,55 +1,50 @@
-output "qumulo_file_systems" {
-  description = "All qumulo_file_system resources"
-  value       = azurerm_qumulo_file_system.qumulo_file_systems
-  sensitive   = true
-}
 output "qumulo_file_systems_admin_password" {
-  description = "List of admin_password values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.admin_password]
+  description = "Map of admin_password values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.admin_password }
   sensitive   = true
 }
 output "qumulo_file_systems_email" {
-  description = "List of email values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.email]
+  description = "Map of email values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.email }
 }
 output "qumulo_file_systems_location" {
-  description = "List of location values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.location]
+  description = "Map of location values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.location }
 }
 output "qumulo_file_systems_name" {
-  description = "List of name values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.name]
+  description = "Map of name values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.name }
 }
 output "qumulo_file_systems_offer_id" {
-  description = "List of offer_id values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.offer_id]
+  description = "Map of offer_id values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.offer_id }
 }
 output "qumulo_file_systems_plan_id" {
-  description = "List of plan_id values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.plan_id]
+  description = "Map of plan_id values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.plan_id }
 }
 output "qumulo_file_systems_publisher_id" {
-  description = "List of publisher_id values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.publisher_id]
+  description = "Map of publisher_id values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.publisher_id }
 }
 output "qumulo_file_systems_resource_group_name" {
-  description = "List of resource_group_name values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.resource_group_name]
+  description = "Map of resource_group_name values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.resource_group_name }
 }
 output "qumulo_file_systems_storage_sku" {
-  description = "List of storage_sku values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.storage_sku]
+  description = "Map of storage_sku values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.storage_sku }
 }
 output "qumulo_file_systems_subnet_id" {
-  description = "List of subnet_id values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.subnet_id]
+  description = "Map of subnet_id values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.subnet_id }
 }
 output "qumulo_file_systems_tags" {
-  description = "List of tags values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.tags]
+  description = "Map of tags values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.tags }
 }
 output "qumulo_file_systems_zone" {
-  description = "List of zone values across all qumulo_file_systems"
-  value       = [for k, v in azurerm_qumulo_file_system.qumulo_file_systems : v.zone]
+  description = "Map of zone values across all qumulo_file_systems, keyed the same as var.qumulo_file_systems"
+  value       = { for k, v in azurerm_qumulo_file_system.qumulo_file_systems : k => v.zone }
 }
 
